61256572e901959b7565814cce3e616633c4b561
Elias AL Shaibani <122492128+EliasALshaibani@users.noreply.github.com>
2025-08-01 03:27:58 +0300

    Update casee.py

--- lawms/overrides/casee.py
+++ lawms/overrides/casee.py
@@ -4,19 +4,17 @@ import frappe
 
 @frappe.whitelist()
 def convert_to_hijri(gregorian_date):
-    """
-    تحويل التاريخ الميلادي إلى هجري باستخدام مكتبة أم القرى
-    :param gregorian_date: التاريخ الميلادي (YYYY-MM-DD)
-    :return: التاريخ الهجري بصيغة YYYY-MM-DD
-    """
+"""
+Convert Gregorian date to Hijri using Umm al-Qura calendar
+:param gregorian_date: Gregorian date (YYYY-MM-DD)
+:return: Hijri date in the format YYYY-MM-DD
+"""
+
     try:
-        # التحقق من صحة الإدخال
         if not gregorian_date:
             frappe.throw("الرجاء إدخال تاريخ ميلادي صالح.")
 
-        # تحويل النص إلى كائن datetime
         gregorian_date_obj = datetime.strptime(gregorian_date, "%Y-%m-%d")
-        # استخدام مكتبة أم القرى للتحويل
         hijri_date = HijriDate(gregorian_date_obj.year, gregorian_date_obj.month, gregorian_date_obj.day, adjust=False)
         return f"{hijri_date.year}-{hijri_date.month:02d}-{hijri_date.day:02d}"
     except Exception as e:
